7c77abad009f6135e731b5c986c73aff269d810e
Wei Zheng <wei.zheng@iress.com>
2020-10-06 14:49:50 +1100

    Exclude and output folder options

--- build_func_deps_dot.py
+++ build_func_deps_dot.py
@@ -2,12 +2,13 @@ import os
 import subprocess
 
 import networkx as nx
+from networkx.exception import NodeNotFound
 from networkx.readwrite.gpickle import read_gpickle
 from networkx.drawing.nx_agraph import write_dot
 
 from build_func_deps import output_file as input_file
 from build_func_deps_config import (
-    func_to_check, upstream_cutoffs, downstream_cutoff, output_folder)
+    func_to_check, funcs_to_exclude, upstream_cutoffs, downstream_cutoff, output_folder)
 
 
 def set_color_of_node(graph, name, color):
@@ -21,37 +22,39 @@ def set_shape_of_node(graph, name, shape):
         graph.nodes[name]['shape'] = shape
 
 
-output_dot_file = os.path.join(output_folder, 'build_func_deps.dot')
-output_png_file = os.path.join(output_folder, 'build_func_deps.png')
+output_dot_file = os.path.join(output_folder, '{}.dot'.format(func_to_check))
+output_png_file = os.path.join(output_folder, '{}.png'.format(func_to_check))
 
 if __name__ == '__main__':
     # networkx 2.2 or above, graphviz or pygraphviz are needed to generate dot & png
 
     call_graph = read_gpickle(input_file)
-    upstream_paths = {}
-    downstream_paths = {}
-    if upstream_cutoffs > 0:
-        upstream_paths = nx.single_target_shortest_path(
-            call_graph, func_to_check, cutoff=upstream_cutoffs)
-    if downstream_cutoff > 0:
-        downstream_paths = nx.single_source_shortest_path(
-            call_graph, func_to_check, cutoff=downstream_cutoff)
-    path_funcs = set()
-    for func_node in upstream_paths.items():
-        path_funcs.update(func_node[1])
-    for func_node in downstream_paths.items():
-        path_funcs.update(func_node[1])
-    if len(path_funcs) > 150:
-        print('More than 150 functions in the graph, please reduce '
-              'upstream_cutoff and/or downstream_cutoff')
-    else:
-        # You can exclude any function names that cause confusing manually here
-        # (note edges will also be excluded), by deleting it from path_funcs
-        call_graph = call_graph.subgraph(path_funcs)
-        if func_to_check in call_graph.nodes:
-            set_color_of_node(call_graph, func_to_check, 'greenyellow')
 
-        write_dot(call_graph, output_dot_file)
-        with open(output_png_file, "wb") as output:
-            subprocess.Popen(
-                ['dot', '-Tpng', output_dot_file], stdout=output)
+    try:
+        upstream_paths = {}
+        downstream_paths = {}
+        if upstream_cutoffs > 0:
+            upstream_paths = nx.single_target_shortest_path(
+                call_graph, func_to_check, cutoff=upstream_cutoffs)
+        if downstream_cutoff > 0:
+            downstream_paths = nx.single_source_shortest_path(
+                call_graph, func_to_check, cutoff=downstream_cutoff)
+        path_funcs = set()
+        for func_node in upstream_paths.items():
+            path_funcs.update(func_node[1])
+        for func_node in downstream_paths.items():
+            path_funcs.update(func_node[1])
+        if len(path_funcs) > 150:
+            print('More than 150 functions in the graph, please reduce '
+                'upstream_cutoff and/or downstream_cutoff')
+        else:
+            call_graph = call_graph.subgraph(path_funcs.difference(funcs_to_exclude))
+
+            if func_to_check in call_graph.nodes:
+                set_color_of_node(call_graph, func_to_check, 'greenyellow')
+            write_dot(call_graph, output_dot_file)
+            with open(output_png_file, "wb") as output:
+                subprocess.Popen(
+                    ['dot', '-Tpng', output_dot_file], stdout=output)
+    except NameError, NodeNotFound:
+        print ('The function "{}" is not found.'.format(func_to_check))
